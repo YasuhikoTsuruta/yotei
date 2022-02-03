2e3d7461c23396f43d6bb8437bc1c9f0da90a9af
YasuhikoTsuruta <96270479+YasuhikoTsuruta@users.noreply.github.com>
2022-02-03 11:29:33 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+// // // // // 'use strict'
+// // // // // // 1行目に記載している 'use strict' は削除しないでください
+
+ function detail () {
+     window.confirm("9:00～12:00　＠Zoom")
+ }
+
+ const target = document.getElementById('schedule1');
+ schedule1.addEventListener('click', detail)
+
+ function detail2 () {
+    window.confirm("9:00～12:00　＠改善場")
+}
+
+const target2 = document.getElementById('schedule2');
+schedule2.addEventListener('click', detail2)
